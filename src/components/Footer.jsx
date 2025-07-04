59bc726e949e37860df2b7e04bd6b1107d27cebc
Abhishek <abhishek.udiya@videonetics.com>
2025-07-04 14:34:41 +0530

    minor change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ export default function Footer() {
             <ul className="space-y-4">
               <li className="flex items-start gap-3">
                 <MapPin className="flex-shrink-0 mt-1" size={20} />
-<span>501, Prince Empire, Race Course Road, Indore, Madhya Pradesh – 452003</span>
+<span>501, Princess Empire, Race Course Road, Indore, Madhya Pradesh – 452003</span>
               </li>
               <li className="flex items-center gap-3">
                 <Mail size={20} />
